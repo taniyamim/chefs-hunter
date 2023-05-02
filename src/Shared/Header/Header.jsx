d0ecf26a922ea3695bccbc61697e21960a47a9e2
taniyamim <jannatulhafsa500@gmail.com>
2023-05-03 00:56:05 +0600

    header nav bar edited logo added

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -3,19 +3,24 @@ import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/esm/Button';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import logo from '../../assets/fusion-cuisine-low-resolution-logo-black-on-transparent-background.png'
+import logo2 from '../../assets/fusion-cuisine-logo.png'
+import './Header.css'
 const Header = () => {
     return (
-        <div className='border container p-4 mt-3'>
-            <h4>header</h4>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <div className='text-center border container p-4 mt-3'>
+
+            <img className='w-25 mb-5' src={logo} alt="" />
+            
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
                 <Container>
-                    <Navbar.Brand href="#home">Fusion Cuisine</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="me-auto">
+                        <Nav className="me-auto d-flex align-items-center">
+                          <img className='logo-img' src={logo2} alt="" />
                             <Nav.Link href="#features">Home</Nav.Link>
                             <Nav.Link href="#pricing">Blog</Nav.Link>
-                            <Nav.Link href="#pricing">Chefs</Nav.Link>
+                            <Nav.Link href="#chefs">Chefs</Nav.Link>
                         </Nav>
                         <Nav>
                             <Nav.Link href="#deets">Profile</Nav.Link>
